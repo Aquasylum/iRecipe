72dcbf044681905f4ee8419e681c057f64552afa
DESKTOP-U804JBD\Hercules PC <hercprogramming@gmail.com>
2022-08-18 16:53:46 +0100

    Refactored and cleaned HeaderComponent

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,12 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { UserDoesNotExist } from '../../validators/UserDoesNotExist.validator';
@@ -10,20 +17,21 @@ import { UserDoesNotExist } from '../../validators/UserDoesNotExist.validator';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(
-    private auth: AuthService,
-    private router: Router,
-    private fb: FormBuilder,
-    private userDoesNotExistValidator: UserDoesNotExist
-  ) {}
   displayName!: string | null | undefined;
   isRecipes: boolean = true;
   userSearchControl!: FormControl;
-  showUserInput: boolean = false;
-  showUserDisplayName: boolean = true;
+  showSettings: boolean = false;
+  showUserInput: boolean = true;
+  currentColorTheme: string = 'dark';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private userDoesNotExistValidator: UserDoesNotExist
+  ) {}
 
   ngOnInit(): void {
-    this.displayName = this.auth.getCurrentUserDisplayName();
+    this.displayName = this.authService.getCurrentUserDisplayName();
     this.userSearchControl = new FormControl(
       '',
       [],
@@ -36,7 +44,7 @@ export class HeaderComponent implements OnInit {
   }
 
   logout() {
-    this.auth
+    this.authService
       .logout()
       .then(() => this.router.navigate(['/login']))
       .catch((e) => console.log(e.message));
@@ -52,11 +60,12 @@ export class HeaderComponent implements OnInit {
     this.router.navigate(['/discover']);
   }
 
-  toggleSearchUserInput() {
-    this.showUserInput = !this.showUserInput;
+  toggleSettings() {
+    this.showSettings = !this.showSettings;
   }
 
-  toggleUserDisplayName() {
-    this.showUserDisplayName = !this.showUserDisplayName;
+  onColorTheme(themeColor: string) {
+    console.log('chaning theme color');
+    this.currentColorTheme = themeColor;
   }
 }
