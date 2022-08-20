17998561208234d49054ba5c375e937b924be479
DESKTOP-U804JBD\Hercules PC <hercprogramming@gmail.com>
2022-08-20 10:57:58 +0100

    Improved functonality for when user is not owner

--- src/app/user/components/profile-header/profile-header.component.ts
+++ src/app/user/components/profile-header/profile-header.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { user } from '@angular/fire/auth';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 import { UserService } from '../../service/user.service';
 
 @Component({
@@ -8,6 +16,9 @@ import { UserService } from '../../service/user.service';
   styleUrls: ['./profile-header.component.css'],
 })
 export class ProfileHeaderComponent implements OnInit {
+  @Input() userId!: string | undefined;
+  @Input() loggedInUserProfile!: boolean;
+
   username: string = '';
   userRating: number = 0;
   userRatingArray: number[] = [];
@@ -15,10 +26,11 @@ export class ProfileHeaderComponent implements OnInit {
   constructor(public userService: UserService) {}
 
   ngOnInit(): void {
-    this.userService.getUserNameAndSurname().then((username) => {
-      username ? (this.username = username?.toLowerCase()) : 'anonymous';
-      console.log(username);
-    });
+    //Check if profile belongs to the logged in user
+    if (this.userId)
+      this.userService.getUserNameAndSurname(this.userId).then((username) => {
+        username ? (this.username = username?.toLowerCase()) : 'anonymous';
+      });
 
     this.userRating = this.userService.getUserRating();
     this.userRatingArray = new Array(this.userRating);
--- src/app/user/components/profile/profile.component.ts
+++ src/app/user/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { Recipe } from 'src/app/recipe/models/Recipe';
 import { RecipeService } from 'src/app/recipe/services/recipe.service';
@@ -12,21 +13,37 @@ export class ProfileComponent implements OnInit {
   allRecipes!: Recipe[];
   recipe!: Recipe;
   recipeIndex: number = 0;
+  userId!: string | undefined;
+  loggedInUserProfile!: boolean;
+  isFavorite: boolean = false;
   constructor(
     private recipeService: RecipeService,
-    private authService: AuthService
+    private authService: AuthService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     if (this.authService.getCurrentUser())
       this.authService.emitCurrentLoggedInStatus(true);
 
-    this.recipeService.getAllRecipesByUserId().then((recipes) => {
-      if (recipes) {
-        this.allRecipes = recipes;
-        this.recipe = this.allRecipes[this.recipeIndex];
-      }
-    });
+    this.userId = this.route.snapshot.paramMap.get('id') as string;
+    if (!this.userId) {
+      this.userId = this.authService.getCurrentUser()?.uid;
+    }
+
+    //Check if this profile belongs to the signed in user:
+    this.userId == this.authService.getCurrentUser()?.uid
+      ? (this.loggedInUserProfile = true)
+      : (this.loggedInUserProfile = false);
+
+    if (this.userId) {
+      this.recipeService.getAllRecipesByUserId(this.userId).then((recipes) => {
+        if (recipes) {
+          this.allRecipes = recipes;
+          this.recipe = this.allRecipes[this.recipeIndex];
+        }
+      });
+    }
   }
 
   nextRecipe() {
@@ -42,5 +59,9 @@ export class ProfileComponent implements OnInit {
       this.recipeIndex = this.allRecipes?.length - 1;
       this.recipe = this.allRecipes[this.recipeIndex];
     }
+  }
+
+  toggleFavorite() {
+    this.isFavorite = !this.isFavorite;
   }
 }
