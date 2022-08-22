e71f6459772c8b951f46a63b37a3475f9d26ed7f
DESKTOP-U804JBD\Hercules PC <hercprogramming@gmail.com>
2022-08-22 07:49:50 +0100

    Added FilterComponent

---
+++ src/app/recipe/components/filter/filter.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { SettingsService } from 'src/app/shared/services/settings.service';
+
+@Component({
+  selector: 'irecipe-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css'],
+})
+export class FilterComponent implements OnInit {
+  @Output() filterData: EventEmitter<string> = new EventEmitter();
+
+  filter!: string;
+  colorTheme: string = 'dark';
+
+  constructor(private settingService: SettingsService) {}
+
+  ngOnInit(): void {
+    this.settingService.colorTheme$.subscribe(
+      (color) => (this.colorTheme = color)
+    );
+  }
+
+  filterRecipes(filter: string) {
+    this.filterData.emit(filter);
+  }
+}
