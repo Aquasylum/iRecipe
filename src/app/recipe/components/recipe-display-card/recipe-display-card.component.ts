aea82cf64a471f4d4a7566d8405c30853b9e3bf6
DESKTOP-U804JBD\Hercules PC <hercprogramming@gmail.com>
2022-08-30 00:36:59 +0100

    Changed DisplayCardComponent look and logic

--- src/app/recipe/components/recipe-display-card/recipe-display-card.component.ts
+++ src/app/recipe/components/recipe-display-card/recipe-display-card.component.ts
@@ -13,6 +13,7 @@ import { RecipeService } from '../../services/recipe.service';
 //Uuid
 import * as uuid from 'uuid';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { UserService } from 'src/app/user/service/user.service';
 
 @Component({
   selector: 'irecipe-recipe-display-card',
@@ -21,20 +22,29 @@ import { AuthService } from 'src/app/auth/services/auth.service';
 })
 export class RecipeDisplayCardComponent implements OnInit, OnChanges {
   @Input() recipe!: Recipe;
+  @Input() loggedInUserProfile!: boolean;
+  @Input() isCarousel!: boolean;
 
   recipeImage!: string | undefined;
   comment: boolean = false;
   like!: boolean;
   userId!: string | undefined;
+  isRecipeAuthor!: boolean;
+
+  isFavorite!: boolean;
 
   constructor(
     private fileService: FileService,
     private recipeService: RecipeService,
-    private authService: AuthService
+    private authService: AuthService,
+    private userService: UserService
   ) {}
 
   ngOnInit(): void {
     this.userId = this.authService.getCurrentUser()?.uid;
+    this.recipe.authorId == this.userId
+      ? (this.isRecipeAuthor = true)
+      : (this.isRecipeAuthor = false);
 
     this.getRecipeImage();
     this.getLike();
@@ -70,13 +80,36 @@ export class RecipeDisplayCardComponent implements OnInit, OnChanges {
   toggleLike() {
     this.like = !this.like;
 
-    if (this.like == true && this.userId) {
-      this.recipe.likedBy.push(this.userId);
-      this.recipeService.updateRecipe(this.recipe);
+    if (
+      this.like == true &&
+      this.userId &&
+      this.recipe.likedBy.indexOf(this.userId) == -1
+    ) {
+      {
+        this.recipe.likedBy.push(this.userId);
+        this.recipeService.updateLikedBy(this.recipe.id, this.userId);
+      }
     }
-    if (this.like == false) {
-      this.recipe.likedBy.filter((item) => item != this.userId);
-      this.recipeService.updateRecipe(this.recipe);
+    if (this.like == false && this.userId) {
+      this.recipe.likedBy.splice(0, 1);
+      this.recipeService.removeLikedBy(this.recipe.id, this.userId);
+    }
+  }
+
+  async toggleFavorite() {
+    this.isFavorite = !this.isFavorite;
+
+    if (this.isFavorite == true) {
+      //Add to this users recipe list
+      this.userService.updateUserWithRecipeId(
+        this.recipe.id,
+        await this.userService.getUsername()
+      );
+    }
+
+    if (this.isFavorite == false) {
+      //Remove from this users recipe list
+      this.userService.deleteRecipeFromUser(this.recipe.id);
     }
   }
 
